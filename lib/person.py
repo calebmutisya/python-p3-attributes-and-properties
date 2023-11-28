5457f13e895cce4db96ea2c2dcae00da094dc0dd
calebmutisya <mutisyacalebmusau@gmail.com>
2023-11-28 12:52:46 +0300

    Completed

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,19 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="", job=""):
+        if name =="":
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+        elif len(str(name))> 25:
+            print("Name must be string between 1 and 25 characters.")
+        elif not 1<= len(str(name)) <=25:
+            self.name = name.title()
+        
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.job = job
+        
+guido = Person("Guido","ITC")
